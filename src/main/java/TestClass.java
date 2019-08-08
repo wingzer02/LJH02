42b5ec9a63238032710e707be8089fd9d4fe37ce
미연시 <woreth@naver.com>
2019-08-08 17:07:05 +0900

    test
    Signed-off-by: 미연시 <woreth@naver.com>

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,21 @@
+
+public class TestClass {
+	int i;
+	String s;
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	public String getS() {
+		return s;
+	}
+
+	public void setS(String s) {
+		this.s = s;
+	}
+}
